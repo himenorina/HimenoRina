9cf4bfb65891003cdb3dd3054128fb616138b96a
HimenoRina <nissy_orange0930@au.com>
2024-01-05 01:09:51 +0900

    初めてのコミット

--- app_project/accounts/models.py
+++ app_project/accounts/models.py
@@ -6,6 +6,8 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from uuid import uuid4
 from datetime import datetime, timedelta
+from django.contrib.auth.models import UserManager
+
 
 class Users(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=255)
@@ -14,11 +16,24 @@ class Users(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     picture = models.FileField(null=True, upload_to='picture/')
 
+    objects = UserManager()
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
     class Meta:
         db_table = 'users'
+
+class UserActivateTokensManager(models.Manager):
+
+    def activate_user_by_token(self, token):
+        user_activate_token = self.filter(
+            token=token,
+            expired_at__gte=datetime.now()
+        ).first()
+        user = user_activate_token.user
+        user.is_active = True
+        user.save()
 
 class UserActivateTokens(models.Model):
 
@@ -28,6 +43,7 @@ class UserActivateTokens(models.Model):
         'Users', on_delete=models.CASCADE
     )
 
+    objects = UserActivateTokensManager()
 
     class Meta:
         db_table = 'user_activate_tokens'
@@ -36,4 +52,5 @@ class UserActivateTokens(models.Model):
 def publish_token(sender, instance, **kwargs):
     user_activate_token = UserActivateTokens.objects.create(
         user=instance, token=str(uuid4()), expired_at=datetime.now() + timedelta(days=1)
-    )       
+    )
+    print(f'http://127.0.0.1:8000/accounts/activate_user/{user_activate_token.token}')     
--- app_project/accounts/views.py
+++ app_project/accounts/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
 from . import forms
 from django.core.exceptions import ValidationError
-# Create your views here.
+from .models import UserActivateTokens
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 def home(request):
     return render(
         request, 'accounts/home.html'
@@ -22,5 +26,35 @@ def regist(request):
     )
 
 def activate_user(request, token):
-    pass
+        user_activate_token = UserActivateTokens.objects.activate_user_by_token(token)
+        return render(
+        request, 'accounts/activate_user.html'
+    )
+    
+def user_login(request):
+    login_form = forms.LoginForm(request.POST or None)
+    if login_form.is_valid():
+        email = login_form.cleaned_data.get('email')
+        password = login_form.cleaned_data.get('password')
+        user = authenticate(email=email, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                messages.success(request, 'ログイン完了しました。')
+                return redirect('accounts:home')
+            else:
+                messages.warning(request, 'ユーザがアクティブでありません')
+        else:
+            messages.warning(request, 'ユーザかパスワードが間違っています')
+    return render(
+        request, 'accounts/user_login.html', context={
+            'login_form': login_form,
+        }
+    )
 
+
+@login_required
+def user_logout(request):
+    logout(request)
+    messages.success(request, 'ログアウトしました')
+    return redirect('accounts:home')
