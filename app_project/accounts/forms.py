65533da0f672a7b24b271ccbab67c3d99d510180
HimenoRina <nissy_orange0930@au.com>
2024-01-05 03:07:26 +0900

    初めてのコミット

--- app_project/accounts/forms.py
+++ app_project/accounts/forms.py
@@ -22,4 +22,28 @@ class RegistForm(forms.ModelForm):
 class LoginForm(forms.Form):
     email = forms.CharField(label="メールアドレス")
     password = forms.CharField(label="パスワード", widget=forms.PasswordInput())
+
+class UserEditForm(forms.ModelForm):
+    username = forms.CharField(label='名前')
+    email = forms.EmailField(label='メールアドレス')
+    picture = forms.FileField(label='写真', required=False)
+
+    class Meta:
+        model = Users
+        fields = ('username', 'email', 'picture')
+
+
+class PasswordChangeForm(forms.ModelForm):
+
+    password = forms.CharField(label='パスワード', widget=forms.PasswordInput())
     
+    class Meta():
+        model = Users
+        fields = ('password', )
+    
+    def save(self, commit=False):
+        user = super().save(commit=False)
+        validate_password(self.cleaned_data['password'], user)
+        user.set_password(self.cleaned_data['password'])
+        user.save()
+        return user
