21897d21c55cc347f338259e3525f6a5eafce0cc
HimenoRina <nissy_orange0930@au.com>
2024-01-25 10:18:11 +0900

    コメント挿入

--- app_project/boards/models.py
+++ app_project/boards/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+class ThemesManager(models.Manager):
+
+    def fetch_all_themes(self):
+        return self.order_by('id').all()
+
 
 class Themes(models.Model):
 
@@ -8,10 +13,16 @@ class Themes(models.Model):
         'accounts.Users', on_delete=models.CASCADE
     )
 
+    objects = ThemesManager()
+
 
     class Meta:
         db_table = 'themes'
 
+
+class CommentsManager(models.Manager):
+    def fetch_by_theme_id(self, theme_id):
+        return self.filter(theme_id=theme_id).order_by('id').all()
 
 class Comments(models.Model):
 
@@ -22,6 +33,7 @@ class Comments(models.Model):
     theme = models.ForeignKey(
         'Themes', on_delete=models.CASCADE
     )
-
+    objects = CommentsManager()
+    
     class Meta:
         db_table = 'comments'
