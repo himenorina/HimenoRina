cb398eb2db732069f18ae4926a9b645c49629783
HimenoRina <nissy_orange0930@au.com>
2024-02-09 10:37:38 +0900

    2

--- app_project/boards/forms.py
+++ app_project/boards/forms.py
@@ -8,16 +8,3 @@ class CreateThemeForm(forms.ModelForm):
     class Meta:
         model = Themes
         fields = ('title',)
-
-class DeleteThemeForm(forms.ModelForm):
-
-    class Meta:
-        model = Themes
-        fields = []
-
-class PostCommentForm(forms.ModelForm):
-    comment = forms.CharField(label='', widget=forms.Textarea(attrs={'rows': 5, 'cols': 60}))
-
-    class Meta:
-        model = Comments
-        fields = ('comment', )
--- app_project/boards/models.py
+++ app_project/boards/models.py
@@ -1,10 +1,5 @@
 from django.db import models
 
-class ThemesManager(models.Manager):
-
-    def fetch_all_themes(self):
-        return self.order_by('id').all()
-
 
 class Themes(models.Model):
 
@@ -13,15 +8,10 @@ class Themes(models.Model):
         'accounts.Users', on_delete=models.CASCADE
     )
 
-    objects = ThemesManager()
 
     class Meta:
         db_table = 'themes'
 
-
-class CommentsManager(models.Manager):
-    def fetch_by_theme_id(self, theme_id):
-        return self.filter(theme_id=theme_id).order_by('id').all()
 
 class Comments(models.Model):
 
@@ -32,7 +22,6 @@ class Comments(models.Model):
     theme = models.ForeignKey(
         'Themes', on_delete=models.CASCADE
     )
-    objects = CommentsManager()
-    
+
     class Meta:
         db_table = 'comments'
